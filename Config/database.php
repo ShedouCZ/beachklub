bba21c6d27b2f9009724f2c3a209f873b0c8122b
unknown <vojta.lanka@gmail.com>
2015-06-19 16:32:31 +0200

    first-commit

---
+++ Config/database.php
@@ -0,0 +1,25 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'beachklub',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+}
