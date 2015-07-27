319ade0b1618a1c93613ccc266f644360ac8fab6
Jan Ptacek <jan.ptacek@gmail.com>
2015-07-27 10:58:28 +0200

    php marks

--- Config/gallery.php
+++ Config/gallery.php
@@ -1,3 +1,4 @@
+<?php
 #### This is just a mark file ###
 Configure::write('GalleryOptions.Pictures.resize_to', array(0, 1024, false));
 Configure::write('GalleryOptions.Pictures.styles.slide', array(926, 0, $cropped=false));
