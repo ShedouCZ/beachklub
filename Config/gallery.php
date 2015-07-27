44d7688757f1b297dd54fda4d523d6bf2e33c031
Jan Ptacek <jan.ptacek@gmail.com>
2015-07-27 11:03:47 +0200

    only medium plus our sizes

--- Config/gallery.php
+++ Config/gallery.php
@@ -1,6 +1,11 @@
 <?php
 #### This is just a mark file ###
 Configure::write('GalleryOptions.Pictures.resize_to', array(0, 1024, false));
-Configure::write('GalleryOptions.Pictures.styles.slide', array(926, 0, $cropped=false));
-Configure::write('GalleryOptions.Pictures.styles.tile', array(309, 153, $cropped=true));
+Configure::write('GalleryOptions.Pictures.styles', 
+	array(
+		'medium' => array(255, 170, $cropped=true),
+		'slide'  => array(926, 0,   $cropped=false),
+		'tile'   => array(309, 153, $cropped=true),
+	) 
+);
 Configure::write('GalleryOptions.Pictures.jpg_quality', 92);
