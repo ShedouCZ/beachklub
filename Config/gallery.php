076738e02e467e09cffa37f4785ace8c4e537450
Jan Ptacek <jan.ptacek@gmail.com>
2015-07-27 10:48:16 +0200

    gallery config

--- Config/gallery.php
+++ Config/gallery.php
@@ -1,1 +1,5 @@
 #### This is just a mark file ###
+Configure::write('GalleryOptions.Pictures.resize_to', array(0, 1024, false));
+Configure::write('GalleryOptions.Pictures.styles.slide', array(926, 0, $cropped=false));
+Configure::write('GalleryOptions.Pictures.styles.tile', array(309, 153, $cropped=true));
+Configure::write('GalleryOptions.Pictures.jpg_quality', 92);
