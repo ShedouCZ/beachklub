d49af47c99e008c64c1f2073fb256156664869c1
Jan Ptacek <jan.ptacek@gmail.com>
2015-10-05 09:07:43 +0200

    public action: add

--- Controller/MessagesController.php
+++ Controller/MessagesController.php
@@ -2,8 +2,13 @@
 App::uses('AppController', 'Controller');
 class MessagesController extends AppController {
 	public $layout = 'BootstrapCake.bootstrap';
-
 	public $components = array('Paginator', 'Session');
+
+	// declare public actions
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('add');
+	}
 
 	public function add() {
 		if ($this->request->is('post')) {
